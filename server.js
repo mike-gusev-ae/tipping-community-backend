d13c449637e2f9d381e5e3e6d45477c5d4458b53
kenodressel <hire@keno.digital>
2020-09-16 10:45:16 +0200

    adds feature documentation

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
 const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const exphbs = require('express-handlebars');
+const { handleResponses, handleRequests } = require('express-oas-generator');
 
 const app = express();
-const exphbs = require('express-handlebars');
-const cors = require('cors');
 const logger = require('./utils/logger')(module);
+
+const openAPIFilePath = './api.json';
 
 // VIEWS
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-app.use(express.json()); // for parsing application/json
 
+app.use(express.json()); // for parsing application/json
+/** handle the responses */
+if (process.env.NODE_ENV !== 'production') {
+  let predefinedSpec;
+
+  try {
+    predefinedSpec = JSON.parse(
+      fs.readFileSync(openAPIFilePath, { encoding: 'utf-8' }),
+    );
+  } catch (e) {
+    //
+  }
+
+  /** work-arounds done. Now handle responses - MUST be the FIRST middleware */
+  handleResponses(app, {
+    specOutputPath: openAPIFilePath,
+    writeIntervalMs: 0,
+    predefinedSpec: predefinedSpec ? () => predefinedSpec : undefined,
+  });
+}
 app.use(cors({
   origin: '*',
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
-
 // ROUTES
 app.use('/blacklist', require('./routes/blacklistRoutes.js'));
 app.use('/comment', require('./routes/commentRoutes.js'));
@@ -33,8 +55,13 @@ app.use('/notification', require('./routes/notificationRoutes.js'));
 
 app.use('/images', express.static('./images'));
 
-app.use((req, res) => {
+/** lastly - add the express-oas-generator request handler (MUST be the LAST middleware) */
+if (process.env.NODE_ENV !== 'production') {
+  handleRequests();
+}
+app.use((req, res, next) => {
   res.sendStatus(404);
+  next();
 });
 
 app.listen(3000, () => {
